3b5a9e18e9decb04336b8c8226442a99de78dc30
ChantYuCN <chengde.yu@intel.com>
2024-03-15 02:07:16 +0000

    support Idempotence

--- install.sh
+++ install.sh
@@ -7,24 +7,23 @@
 # step -1. env var
 IP=$1
 DNS=$2  # need include quote
-mkdir -p _workspace
-pushd _workspace
 #curl -O https://concourse-ci.org/docker-compose.yml
-if [ -z IP ] || [ -z DNS ]; then
+if [ -z $IP ] || [ -z $DNS ]; then
   echo "please provide host ip and dns ip/url"
   exit
 fi
 
 # step 0. install docker and docker-compose
-sudo apt-get install docker.io -y
-sudo usermod -aG docker $(users)
-#sudo newgrp docker
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+#sudo apt-get install docker.io -y
+#sudo usermod -aG docker $(users)
+##sudo newgrp docker
+#sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#sudo chmod +x /usr/local/bin/docker-compose
 
 
 # step 1. Clean the redundant data
-if [[ -d _workspace ]]; then
+if [[ $(docker ps -f "ancestor=concourse/concourse") ]]; then
+  echo "Clean the redundant data"
   pushd _workspace
   docker-compose down
   popd
@@ -37,17 +36,31 @@ if [[ -f /usr/local/bin/fly ]]; then
   sudo rm -rf /usr/local/bin/fly 
 fi
 
-
 # step 2. make sure consourse port is empty
-inuse=$(sudo lsof  -i -P -n  | grep LISTEN | grep 8080)
-if [[ ! -z $inuse ]]; then
-  	echo "plz make sure the port 8080 is not occupied"
-	exit
-fi
+until ! [[ $(sudo lsof  -i -P -n  | grep LISTEN | grep 8080) ]]
+do
+  echo "wait to refresh env"
+  sleep 1
+done
+#t=0
+#until [ ! $t -lt 60 ]
+#do
+#inuse=$(sudo lsof  -i -P -n  | grep LISTEN | grep 8080)
+#sleep 1
+#echo $t
+#t=`expr $t +1`
+#if [[ ! -z $inuse ]]; then
+#  	echo "plz make sure the port 8080 is not occupied"
+#fi
+#done
+#exit
 
-# step 2. install concourse and fly
+
+
+# step 3. install concourse and fly
 # concourse contains TSA(seucrity), ATC(UI), gardon(container manager), baggageclaim(volume)
 # db 
+pushd _workspace
 host_ip=$IP worker_dns=$DNS docker-compose up -d
 popd
 
