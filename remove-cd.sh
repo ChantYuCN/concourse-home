38d0b5de9a67fbbb25ef36fd6ee07b24bbaae0a2
ChantYuCN <chengde.yu@intel.com>
2024-03-21 10:21:41 +0000

    update the script

--- remove-cd.sh
+++ remove-cd.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+FULL=$1
+
 sudo kill -9  $(ps -aux | grep 8083 | awk '{print $2}' | head -n 1)
+
+if [[ $FULL == "yes" ]]; then
+  kubectl delete -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+  kubectl delete ns argocd
+  rm -rf /usr/local/bin/argocd
+fi
+
